5430ae953904a75880cc8759d98462936e1fdb1b
Michael Chacon <mike@sicdigital.com>
2014-08-25 01:35:23 -0400

    First commit of working environment

---
+++ environment/puppet/modules/apache/manifests/init.pp
@@ -0,0 +1,24 @@
+class apache {
+
+  # install apache
+  package { "apache2":
+    ensure => present,
+    require => Exec["apt-get update"]
+  }
+
+  # ensures that mode_rewrite is loaded and modifies the default configuration file
+  file { "/etc/apache2/mods-enabled/rewrite.load":
+    ensure => link,
+    target => "/etc/apache2/mods-available/rewrite.load",
+    require => Package["apache2"]
+  }
+
+  # starts the apache2 service once the packages installed, and monitors changes to its configuration files and reloads if nesessary
+  service { "apache2":
+    ensure => running,
+    require => Package["apache2"],
+    subscribe => [
+      File["/etc/apache2/mods-enabled/rewrite.load"],
+    ],
+  }
+}
